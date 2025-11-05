cf3007139d236b99d544e7f7970ece87b3ee7a60
Pandraghon <pandraghon@outlook.com>
2025-11-05 08:26:32 +0000

    chore(scripts): pull images before starting containers

--- start_prod.sh
+++ start_prod.sh
@@ -1,4 +1,4 @@
 export NODE_ENV=production
 export OGDEN_VERSION=stable
 export COMPOSE_PROJECT_NAME=ogden
-docker compose -f docker-compose.yml up -d
+docker compose pull && docker compose -f docker-compose.yml up -d
